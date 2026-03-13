562d34e6048dd702575a30df4c7722395506f064
jesuslitrus <jesuslitrus@gmail.com>
2026-03-13 18:05:56 +0100

    update

--- components/SettingsMenu.js
+++ components/SettingsMenu.js
@@ -40,13 +40,14 @@ return (
 const styles = StyleSheet.create({
 
   overlay:{
-    position:"absolute",
-    top:0,
-    left:0,
-    right:0,
-    bottom:0,
-    zIndex:1000
-  },
+  position:"absolute",
+  top:0,
+  left:0,
+  right:0,
+  bottom:0,
+  backgroundColor:"rgba(0,0,0,0.2)",
+  zIndex:1000
+},
 
   background:{
     position:"absolute",
