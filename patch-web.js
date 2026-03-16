56fad7af6862ffa649acb08dedb1a13c14291f80
jesuslitrus <jesuslitrus@gmail.com>
2026-03-16 23:16:29 +0100

    update

--- patch-web.js
+++ patch-web.js
@@ -9,26 +9,17 @@ execSync("npx expo export --platform web", { stdio: "inherit" });
 let html = fs.readFileSync("dist/index.html", "utf8");
 html = html.replace(/\/_expo/g, "_expo");
 
-// 3 insertar meta tag iOS
-if (!html.includes("apple-mobile-web-app-capable")) {
-  html = html.replace(
-    "<head>",
-    `<head>
-<meta name="apple-mobile-web-app-capable" content="yes">`
-  );
-}
-
 fs.writeFileSync("dist/index.html", html);
 
-// 4 borrar docs
+// 3 borrar docs
 if (fs.existsSync("docs")) {
   fs.rmSync("docs", { recursive: true, force: true });
 }
 
-// 5 renombrar dist -> docs
+// 4 renombrar dist -> docs
 fs.renameSync("dist", "docs");
 
-// 6 copiar assets
+// 5 copiar assets
 if (!fs.existsSync("docs/assets")) {
   fs.mkdirSync("docs/assets", { recursive: true });
 }
@@ -36,29 +27,26 @@ if (!fs.existsSync("docs/assets")) {
 fs.copyFileSync("assets/logo_192.png", "docs/assets/logo_192.png");
 fs.copyFileSync("assets/logo_512.png", "docs/assets/logo_512.png");
 
-// 7 parche adicional index.html
+// 6 parche adicional index.html
 let docHtml = fs.readFileSync("docs/index.html", "utf8");
 
 const snippet = `
-<link rel="manifest" href="manifest.json">
 <link rel="apple-touch-icon" href="assets/logo_192.png">
-<meta name="apple-mobile-web-app-status-bar-style" content="default">
-<meta name="apple-mobile-web-app-title" content="Ubica-Pin">
 <meta name="theme-color" content="#ffffff">
 `;
 
-if (!docHtml.includes("apple-mobile-web-app-title")) {
+if (!docHtml.includes("apple-touch-icon")) {
   docHtml = docHtml.replace("</head>", snippet + "\n</head>");
 }
 
 fs.writeFileSync("docs/index.html", docHtml);
 
-// 8 crear .nojekyll
+// 7 crear .nojekyll
 fs.writeFileSync("docs/.nojekyll", "");
 
 console.log("Web preparada");
 
-// 9 git commit + push
+// 8 git commit + push
 execSync("git add .", { stdio: "inherit" });
 execSync('git commit -m "update"', { stdio: "inherit" });
 execSync("git push", { stdio: "inherit" });
