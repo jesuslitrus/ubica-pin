2e2175668274b66f0ab962819859b4ff8185cfa4
jesuslitrus <jesuslitrus@gmail.com>
2026-03-16 23:51:49 +0100

    update

--- patch-web.js
+++ patch-web.js
@@ -34,8 +34,6 @@ const snippet = `
 
 <link rel="apple-touch-icon" href="assets/logo_192.png">
 
-<meta name="apple-mobile-web-app-capable" content="yes">
-<meta name="apple-mobile-web-app-title" content="Ubica-Pin">
 <meta name="apple-mobile-web-app-status-bar-style" content="default">
 
 <link rel="apple-touch-icon" href="assets/logo_192.png">
